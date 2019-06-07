8fd559a0575f1b2aaa7e128295a74dbe90c160a4
Claudia Solbes <cssolbes@hotmail.com>
2019-06-07 00:24:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,20 +6,20 @@ def array_with_two_elements
   array = ["1","2"]
 end
 
-def first_element
+def first_element(array)
   array[0]
 end
 
-def third_element
+def third_element(array2)
   array2 = ["1", "2", "3", "4"]
-  array[2]
+  array2[2]
 end
 
-def last_element
+def last_element(array2)
   array2[-1]
 end
 
-def first_element_with_array_methods
+def first_element_with_array_methods(array)
   array.first
 end
   
